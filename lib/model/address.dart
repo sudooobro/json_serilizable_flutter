5c2a5b7e12995fe7e3de787e94960cba56ccc0c0
sudooobro <sudooobro@gmail.com>
2020-12-09 15:20:10 +0500

    files

--- lib/model/address.dart
+++ lib/model/address.dart
@@ -2,13 +2,16 @@ import 'package:json_annotation/json_annotation.dart';
 
 //part <file name> must be like this <file name> with <.g.> in center
 part 'address.g.dart';
+//RUN IN TERMINAL : flutter pub run build_runner build
 
 @JsonSerializable()
 class Address {
   //dont use final
   String street, landmark, city, state;
   Address({this.street, this.landmark, this.city, this.state});
+
   factory Address.fromJson(Map<String, dynamic> data) =>
       _$AddressFromJson(data);
+
   Map<String, dynamic> toJson() => _$AddressToJson(this);
 }
